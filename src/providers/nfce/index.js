74ef1e1e11fd16e05b8d19f941070d386c57a658
Pedro Tashima <pedrotashima42@gmail.com>
2022-02-20 19:07:51 -0300

    feat(pr): add  nfKey, sellerInfo, nfInfo, consumer

--- src/providers/nfce/index.js
+++ src/providers/nfce/index.js
@@ -4,6 +4,8 @@ import buildPr from "./pr.js"
 
 const pr = buildPr({http, htmlParser})
 
+//pr.getNfce({url: "http://www.fazenda.pr.gov.br/nfce/qrcode?p=41220176189406000983651180003585831191202186|2|1|1|5CE86D7F913341DD661F9D5F606DD4C3E21647AF"})
+
 const nfce = {
   pr,
 }
